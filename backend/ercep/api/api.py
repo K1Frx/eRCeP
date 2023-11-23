ace271640f37b3c944df8746f2347f9302634f86
Kamil Fryszkowski <kamil.fryszkowski@idealgroup.com.pl>
2023-11-23 19:57:37 +0100

    API and serializers

---
+++ backend/ercep/api/api.py
@@ -0,0 +1,69 @@
+from genericapi import GenericCRUDAPIView
+from models import Worker, Employer, Contract, Absence, AbsenceType, Worktime
+from serializers import *
+
+class WorkerAPIView(GenericCRUDAPIView):
+    model = Worker
+    
+    request_serializer_get = WorkerGetRequestSerializer
+    request_serializer_post = WorkerPostRequestSerializer
+    request_serializer_patch = WorkerPatchRequestSerializer
+    
+    response_serializer_get = WorkerResponseSerializer
+    response_serializer_post = WorkerResponseSerializer
+    response_serializer_patch = WorkerResponseSerializer
+    
+class EmployerAPIView(GenericCRUDAPIView):
+    model = Employer
+    
+    request_serializer_get = EmployerGetRequestSerializer
+    request_serializer_post = EmployerPostRequestSerializer
+    request_serializer_patch = EmployerPatchRequestSerializer
+    
+    response_serializer_get = EmployerResponseSerializer
+    response_serializer_post = EmployerResponseSerializer
+    response_serializer_patch = EmployerResponseSerializer
+
+class ContractAPIView(GenericCRUDAPIView):
+    model = Contract
+    
+    request_serializer_get = ContractGetRequestSerializer
+    request_serializer_post = ContractPostRequestSerializer
+    request_serializer_patch = ContractPatchRequestSerializer
+    
+    response_serializer_get = ContractResponseSerializer
+    response_serializer_post = ContractResponseSerializer
+    response_serializer_patch = ContractResponseSerializer
+
+class AbsenceAPIView(GenericCRUDAPIView):
+    model = Absence
+    
+    request_serializer_get = AbsenceGetRequestSerializer
+    request_serializer_post = AbsencePostRequestSerializer
+    request_serializer_patch = AbsencePatchRequestSerializer
+    
+    response_serializer_get = AbsenceResponseSerializer
+    response_serializer_post = AbsenceResponseSerializer
+    response_serializer_patch = AbsenceResponseSerializer
+
+class AbsenceTypeAPIView(GenericCRUDAPIView):
+    model = AbsenceType
+    
+    request_serializer_get = AbsenceTypeGetRequestSerializer
+    request_serializer_post = AbsenceTypePostRequestSerializer
+    request_serializer_patch = AbsenceTypePatchRequestSerializer
+    
+    response_serializer_get = AbsenceTypeResponseSerializer
+    response_serializer_post = AbsenceTypeResponseSerializer
+    response_serializer_patch = AbsenceTypeResponseSerializer
+
+class WorktimeAPIView(GenericCRUDAPIView):
+    model = Worktime
+    
+    request_serializer_get = WorktimeGetRequestSerializer
+    request_serializer_post = WorktimePostRequestSerializer
+    request_serializer_patch = WorktimePatchRequestSerializer
+    
+    response_serializer_get = WorktimeResponseSerializer
+    response_serializer_post = WorktimeResponseSerializer
+    response_serializer_patch = WorktimeResponseSerializer
---
+++ backend/ercep/api/serializers.py
@@ -0,0 +1,123 @@
+from rest_framework import serializers
+from models import Worker, Employer, Contract, Absence, AbsenceType, Worktime
+
+# Generic serializers
+class GenericPaginationResponseSerializer(serializers.Serializer):
+    page = serializers.IntegerField()
+    total = serializers.IntegerField()
+    per_page = serializers.IntegerField()
+    num_pages = serializers.IntegerField()
+    offset = serializers.IntegerField()
+    
+class GenericPaginationRequestSerializer(serializers.Serialize):
+    page = serializers.IntegerField(required=False)
+    per_page = serializers.IntegerField(required=False)
+    
+# Worker serializers
+class WorkerGetRequestSerializer(GenericPaginationRequestSerializer):
+    first_name = serializers.ListField(child=serializers.CharField(max_length=50), required=False)
+    last_name = serializers.ListField(child=serializers.CharField(max_length=50), required=False)
+    email = serializers.ListField(child=serializers.EmailField(), required=False)
+    phone_number = serializers.ListField(child=serializers.CharField(max_length=20), required=False)
+    birth_date = serializers.ListField(child=serializers.DateField(), required=False)
+    
+class WorkerPostRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Worker
+        fields = '__all__'
+        
+class WorkerPatchRequestSerializer(WorkerPostRequestSerializer):
+    pass
+
+class WorkerResponseSerializer(GenericPaginationResponseSerializer):
+    items = WorkerPostRequestSerializer(many=True)
+
+# Employer serializers
+class EmployerGetRequestSerializer(GenericPaginationRequestSerializer):
+    name = serializers.ListField(child=serializers.CharField(max_length=50), required=False)
+    nip = serializers.ListField(child=serializers.CharField(max_length=10), required=False)
+    adress = serializers.ListField(child=serializers.CharField(max_length=100), required=False)
+
+class EmployerPostRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employer
+        fields = '__all__'
+
+class EmployerPatchRequestSerializer(EmployerPostRequestSerializer):
+    pass
+
+class EmployerResponseSerializer(GenericPaginationResponseSerializer):
+    items = EmployerPostRequestSerializer(many=True)
+    
+# Contract serializers
+class ContractGetRequestSerializer(GenericPaginationRequestSerializer):
+    employer = serializers.ListField(child=serializers.IntegerField(), required=False)
+    worker = serializers.ListField(child=serializers.IntegerField(), required=False)
+    date_start = serializers.ListField(child=serializers.DateField(), required=False)
+    date_end = serializers.ListField(child=serializers.DateField(), required=False)
+
+class ContractPostRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = '__all__'
+
+class ContractPatchRequestSerializer(ContractPostRequestSerializer):
+    pass
+
+class ContractResponseSerializer(GenericPaginationResponseSerializer):
+    items = ContractPostRequestSerializer(many=True)
+
+# Absence serializers
+class AbsenceGetRequestSerializer(GenericPaginationRequestSerializer):
+    worker = serializers.ListField(child=serializers.IntegerField(), required=False)
+    contract = serializers.ListField(child=serializers.IntegerField(), required=False)
+    date = serializers.ListField(child=serializers.DateField(), required=False)
+    absence_type = serializers.ListField(child=serializers.IntegerField(), required=False)
+    comment = serializers.ListField(child=serializers.CharField(max_length=500), required=False)
+    
+class AbsencePostRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Absence
+        fields = '__all__'
+        
+class AbsencePatchRequestSerializer(AbsencePostRequestSerializer):
+    pass
+
+class AbsenceResponseSerializer(GenericPaginationResponseSerializer):
+    items = AbsencePostRequestSerializer(many=True)
+
+# AbsenceType serializers
+class AbsenceTypeGetRequestSerializer(GenericPaginationRequestSerializer):
+    name = serializers.ListField(child=serializers.CharField(max_length=50), required=False)
+    comment = serializers.ListField(child=serializers.CharField(max_length=500), required=False)
+    
+class AbsenceTypePostRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AbsenceType
+        fields = '__all__'
+        
+class AbsenceTypePatchRequestSerializer(AbsenceTypePostRequestSerializer):
+    pass
+
+class AbsenceTypeResponseSerializer(GenericPaginationResponseSerializer):
+    items = AbsenceTypePostRequestSerializer(many=True)
+
+# Worktime serializers
+class WorktimeGetRequestSerializer(GenericPaginationRequestSerializer):
+    worker = serializers.ListField(child=serializers.IntegerField(), required=False)
+    contract = serializers.ListField(child=serializers.IntegerField(), required=False)
+    time_in = serializers.ListField(child=serializers.DateTimeField(), required=False)
+    time_out = serializers.ListField(child=serializers.DateTimeField(), required=False)
+    sum_min = serializers.ListField(child=serializers.IntegerField(), required=False)
+    comment = serializers.ListField(child=serializers.CharField(max_length=500), required=False)
+    
+class WorktimePostRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Worktime
+        fields = '__all__'
+        
+class WorktimePatchRequestSerializer(WorktimePostRequestSerializer):
+    pass
+
+class WorktimeResponseSerializer(GenericPaginationResponseSerializer):
+    items = WorktimePostRequestSerializer(many=True)
