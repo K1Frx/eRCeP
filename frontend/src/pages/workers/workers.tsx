86712d53e74a576b5525da2a372ad2682c30731f
Colty1999 <mateuszgietka99@gmail.com>
2024-01-10 02:18:47 +0100

    workers API download and display handle

--- frontend/src/pages/workers/workers.tsx
+++ frontend/src/pages/workers/workers.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../App";
 import "./workers.scss";
 import axios from "axios";
@@ -9,7 +9,8 @@ import Table from "react-bootstrap/esm/Table";
 const Workers = () => {
     const { setLoading } = useContext(AppContext);
     let loginToken = useStorageState({ state: "loginToken" });
-
+    const tableHeaders = ["ID", "Name", "Surname", "Birth date", "Email", "Phone", "Contracts"]
+    const [workers, setWorkers] = useState<any[]>([]);
 
     useEffect(() => {
         setLoading(true);
@@ -19,7 +20,8 @@ const Workers = () => {
                     Authorization: `Bearer ${loginToken.store}`
                 }
             }).then((res) => {
-                console.log(res);
+                console.log(res.data.items);
+                setWorkers(res.data.items);
                 // setError(false);
                 // setDisabled(false);
                 setLoading(false);
@@ -35,34 +37,28 @@ const Workers = () => {
 
     return (
         <div className="workersContainer">
-            <Table responsive>
+            <Table >
                 <thead>
-                    <tr>
-                        <th>#</th>
-                        {Array.from({ length: 12 }).map((_, index) => (
-                            <th key={index}>Table heading</th>
+                    <tr className="tableHeader">
+                        {tableHeaders.map((header, index) => (
+                            <th key={index}>{header}</th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        {Array.from({ length: 12 }).map((_, index) => (
-                            <td key={index}>Table cell {index}</td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        {Array.from({ length: 12 }).map((_, index) => (
-                            <td key={index}>Table cell {index}</td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        {Array.from({ length: 12 }).map((_, index) => (
-                            <td key={index}>Table cell {index}</td>
-                        ))}
-                    </tr>
+                    {workers.map((worker, index) => (
+                        <tr key={index}>
+                            <td>{worker.id}</td>
+                            <td>{worker.first_name}</td>
+                            <td>{worker.last_name}</td>
+                            <td>{worker.birth_date}</td>
+                            <td>{worker.email}</td>
+                            <td>{worker.phone_number}</td>
+                            <td>{worker.contracts ?? "empty"}</td> 
+                            {/* items have unused user value */}
+                        </tr>
+                    ))
+                    }
                 </tbody>
             </Table>
         </div>
